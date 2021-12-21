34ba42c77931d62849035f76dac8833609e98ef3
Infinitemarcus <marcus.natrielli@outlook.com>
2021-12-21 15:03:54 -0300

    Adding C copy text file (with argv)

---
+++ c/copy text file.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	
+	char sourceFile[50], destinationFile[50];
+	
+	if(!argv[1] || !argv[2]) {
+		printf("Required arguments not provided");
+		return 0;
+	}
+
+	strcpy(sourceFile, argv[1]);
+	strcat(sourceFile, ".txt");
+	
+	strcpy(destinationFile, argv[2]);
+	strcat(destinationFile, ".txt");
+	
+	printf("File names: %s %s\n", sourceFile, destinationFile);
+	
+	FILE * source = fopen(sourceFile, "r");
+	FILE * destination = fopen(destinationFile, "w");
+	
+	if(source == NULL || destination == NULL) {
+		printf("Error when opening files");
+		return 0;
+	}
+	
+	char c = fgetc(source);
+	
+	do {
+		if((c <= 'z' && c>= 'a') || (c <= 'Z' && c >= 'A'))
+			fputc(c, destination);
+			
+		c = fgetc(source);
+	} while(c != EOF);
+	
+	return 0;
+}
+
